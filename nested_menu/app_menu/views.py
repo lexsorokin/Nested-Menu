a559f2b0d8f550e4245da1e8c7c402a897699a3d
Alexander Sorokin <lex.sorokin.dev@gmail.com>
2023-03-06 21:43:09 +0300

    HTML static fix

--- nested_menu/app_menu/views.py
+++ nested_menu/app_menu/views.py
@@ -7,3 +7,6 @@ class CategoryListView(ListView):
     model = Category
     template_name = "menu/category_list.html"
     context_object_name = 'categories'
+
+    # def get_context_data(self, *, object_list=None, **kwargs):
+    #     context = super(Category, self).get_context_data()
